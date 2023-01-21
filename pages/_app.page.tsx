01bc22ae12d35c3ae66bef69a9e34d408fa04b24
Yurdanov <yurdanov@gnivc.ru>
2023-01-21 12:27:40 +0300

    add loading _app

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from 'react';
+import Router from 'next/router';
 import type { AppProps } from 'next/app';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import 'bootstrap/dist/css/bootstrap.css';
 
+import { Loader, Layout } from 'components';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [loading, setLoading] = useState(false);
+
+  const start = () => setLoading(true);
+
+  const end = () => setLoading(false);
+
+  useEffect(() => {
+    Router.events.on('routeChangeStart', start);
+    Router.events.on('routeChangeComplete', end);
+    Router.events.on('routeChangeError', end);
+    return () => {
+      Router.events.off('routeChangeStart', start);
+      Router.events.off('routeChangeComplete', end);
+      Router.events.off('routeChangeError', end);
+    };
+  }, []);
+  
+  return loading 
+    ? (
+      <Layout>
+        <Loader />
+      </Layout>
+    )
+    : <Component {...pageProps} />;
 }
